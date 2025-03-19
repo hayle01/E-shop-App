5fa238fe3a9e7f3b9994e980c76e2b37a08aa7fe
Hayle01 <mabdirahim832@gmail.com>
2025-03-19 20:29:10 +0300

    Fetching products from API

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import productData from '../assets/ProductData.json'
+import { ProductItem } from './ProductItem';
+import axios from 'axios';
+import { ProductLoadSekeleton } from './ProductLoadSekeleton';
 export const ProductsList = () => {
-    console.log(productData);
-  return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
-        {
-            productData.map(product => (
-                <div className=''>
-
-                </div>
-            ))
+  const [products, setProducts] = useState([])
+  const [Loading, setLoading] = useState(false)
+    useEffect(() => {
+      const fetchProduct = async () =>{
+        try {    
+          setLoading(true)
+          const { data } = await axios.get(
+            "https://dummyjson.com/products?limit=100"
+          );
+            setProducts(data.products)
+            setLoading(false)
+        } catch (error) {
+          setLoading(false)
+          console.log(error.message);
         }
+      }
+      fetchProduct();
+    }, [])
+
+    if(Loading) return <ProductLoadSekeleton />
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+      { products.length > 0 && products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
     </div>
-  )
+  );
 }
